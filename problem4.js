53c87e64046ecd0ae67a52a2aba1fcff77c1f79f
Badilisha Productions <badilishaproductions@Badilishas-MacBook-Pro.local>
2022-10-06 11:14:00 +0300

    Problem 4

--- problem4.js
+++ problem4.js
@@ -3,50 +3,55 @@
 //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is, 2640
 //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-// function difference(number){
-//     for(let i = 1; i <= number; i++){
-//         let sum, square , diff, numbersq, initialSum;
-//         numbersq = number ** 2
-//         sum += numbersq
-//         initialSum += number
-//         square = initialSum ** 2
-//         diff = square - sum
-//         console.log(diff)
-//     }
+function difference(number){
+    let sum, square , diff;
+    sum = (
+        (number*(number+1)) * (number+(number+1))
+    ) / 6
+    square = Math.pow( (number+1)*(number/2), 2)
+    diff = square - sum
+    console.log(diff)
+}
+
+difference(100)
+
+
+
+
+
+
+2.
+
+
+// // list of numbers we wanna test
+// let test_values = [10, 20, 100];
+
+// // this function execute the code and records the time to execute
+// function run_function(func, test_values) {
+//   for(let i in test_values){
+//     console.log('Test value:', test_values[i]);
+//     var t0 = performance.now();
+//     console.log('Output:', func(test_values[i]));
+//     var t1 = performance.now();
+//     console.log("Took " + (t1 - t0) + " ms");
+//     console.log();
+//   }
 // }
 
-// difference(10)
+// function sumSquareDifference(n) {
+//   return square_sum_of_first_n(n) - sum_of_square(n);
+// }
 
+// function sum_of_square(n){
+//   return (
+//     (
+//       (n * (n+1)) *
+//       (n + (n+1))
+//     )/6)
+// }
 
-// list of numbers we wanna test
-let test_values = [10, 20, 100];
+// function square_sum_of_first_n(n){
+//   return Math.pow( (n+1)*(n/2), 2);
+// }
 
-// this function execute the code and records the time to execute
-function run_function(func, test_values) {
-  for(let i in test_values){
-    console.log('Test value:', test_values[i]);
-    var t0 = performance.now();
-    console.log('Output:', func(test_values[i]));
-    var t1 = performance.now();
-    console.log("Took " + (t1 - t0) + " ms");
-    console.log();
-  }
-}
-
-function sumSquareDifference(n) {
-  return square_sum_of_first_n(n) - sum_of_square(n);
-}
-
-function sum_of_square(n){
-  return (
-    (
-      (n * (n+1)) *
-      (n + (n+1))
-    )/6)
-}
-
-function square_sum_of_first_n(n){
-  return Math.pow( (n+1)*(n/2), 2);
-}
-
-run_function(sumSquareDifference, test_values);
+// run_function(sumSquareDifference, test_values);
